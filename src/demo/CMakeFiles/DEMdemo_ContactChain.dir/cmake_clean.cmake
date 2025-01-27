4cfde8b1b1015169a795069342ce97142125988b
Lukas C Garbacik <lukasg@csrwks2023-0217.engin.umich.edu>
2025-01-27 17:14:24 -0500

    build demos

---
+++ src/demo/CMakeFiles/DEMdemo_ContactChain.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/DEMdemo_ContactChain"
+  "../../bin/DEMdemo_ContactChain.pdb"
+  "CMakeFiles/DEMdemo_ContactChain.dir/DEMdemo_ContactChain.cpp.o"
+  "CMakeFiles/DEMdemo_ContactChain.dir/DEMdemo_ContactChain.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DEMdemo_ContactChain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/demo/CMakeFiles/DEMdemo_ContactChain.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DEMdemo_ContactChain.
+# This may be replaced when dependencies are built.
